3f7177ac096f14388b05264f9897857528323781
Nizar Fadilah <nizarfadilah74@gmail.com>
2025-06-29 23:28:35 +0700

    fix: fix configuration main program and add gorm/gofiber configuration

--- main.go
+++ main.go
@@ -2,24 +2,37 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gofiber/fiber/v2"
 	"github.com/imnzr/northern-gaming-club-api/database"
 )
 
 func main() {
-	// env := godotenv.Load()
-	// if env != nil {
-	// 	fmt.Println("env file is not loaded")
-	// }
 
-	db, err := database.DatabaseConnection()
-	if err != nil {
+	// Database Configuration START
+	db, errDb := database.DatabaseConnection()
+	if errDb != nil {
 		fmt.Println("connection to database failed")
 	}
-
 	defer db.Close()
-
 	fmt.Println("connection to database successfully")
+	// Database Configuration FINISH
 
+	// HTTP SERVER
+	app := fiber.New(fiber.Config{
+		IdleTimeout:  time.Second * 5,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+	})
+	// TEST HELLO WORLD
+	app.Get("/", func(controller *fiber.Ctx) error {
+		return controller.SendString("Hello World")
+	})
+
+	err := app.Listen("localhost:8080")
+	if err != nil {
+		panic(err)
+	}
 }
